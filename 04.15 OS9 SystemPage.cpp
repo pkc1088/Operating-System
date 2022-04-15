b5c49ce8dcd1f791000324a89f987d61ba726e63
pkc1088 <84122393+pkc1088@users.noreply.github.com>
2022-04-15 22:05:44 +0900

    Add files via upload

---
+++ 04.15 OS9 SystemPage.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    //to know the size of system page
+    //use "getpagesize()" system call or use a command
+    //command : getconf PAGESIZE
+    //output : 4096
+    
+    return 0;
+}
